cb5e9051ca7a81903002f5cc80b02e4abb1052ab
thinhemb <thinhdo19022001@gmail.com>
2022-06-04 18:56:27 +0700

    fix path model

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if __name__=='__main__':
     ws=st.text_input("Width  of the sepal (in cm)",placeholder="4.0 -> 7.9")
     lp=st.text_input("Length of the petal (in cm)",placeholder="1.0 -> 6.9")
     wp=st.text_input("Width  of the petal (in cm)",placeholder="0.1 -> 2.6")
-    path_model="DecisionTreeID/model.pkl"
+    path_model="./model.pkl"
     model = pickle.load(open(path_model, 'rb'))
     if st.button('Predicted') :
         sample={'sepal_length': float(ls), 'sepal_width': float(ws), 'petal_length': float(lp), 'petal_width': float(wp)}
